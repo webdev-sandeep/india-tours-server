3a893e8793860cb02cfe601a37d6831f0585a750
sandeep <sandeepkumar13975@gmail.com>
2022-06-03 12:27:36 +0530

    port changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import tourRouter from './routes/tour.js'
 import dotenv from 'dotenv'
 
 const app = express()
-const port = process.env.PORT || 5000
+
 dotenv.config()
 
 app.use(morgan('dev'))
@@ -26,6 +26,6 @@ app.get('/',(req,res)=>{
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
-        app.listen(port,()=>console.log(`Listening to the server at : http://localhost:${port}`))
+        app.listen(process.env.PORT || 5000,()=>console.log(`Listening to the server at : http://localhost:${port}`))
     })
     .catch((error)=>console.log(error))
